3923762a7f2f972992133bfd1eea157cb870b2f0
Nikita Stetskiy <nikin929@gmail.com>
2021-02-12 14:24:35 +0100

    Adding a proper header and particles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components';
 function App() {
   return (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <CssBaseline />
       <Home />
     </>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,66 @@
+/* global makeStyles */
 import React from 'react';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/styles';
 import Navbar from './Navbar';
+import Header from './Header';
 
-const Home = () => (
-  <div>
-    <Navbar />
-  </div>
-);
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: 'absolute',
+  },
+});
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 1,
+                color: 'blue',
+              },
+            },
+            size: {
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 7,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true,
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default Home;
