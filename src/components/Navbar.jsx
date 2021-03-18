2dca0d06087b93743e4325c7944dad33e41b902b
Nikita Stetskiy <nikin929@gmail.com>
2021-03-18 12:42:49 +0100

    Adding Bootstrap Navbar #1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,42 @@
+/* eslint-disable no-undef */
 import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { mainBody, about } from '../editable-stuff/config';
+import pdf from '../editable-stuff/resume.pdf';
 
 const Navigation = React.forwardRef(() => {
-  const [isTop] = useState(true);
+  const isTop = window.pageYOffset;
+  console.log(window.pageYOffset);
   const navbarMenuRef = React.useRef();
 
   return (
     <Navbar
       ref={navbarMenuRef}
       className={` fixed-top  ${
-        !isTop ? 'navbar-white' : 'navbar-transparent'
+        window.pageYOffset ? 'transparent' : 'navbar-white'
       }`}
       expand="lg"
     >
-      <Navbar.Brand className="brand" href={`${process.env.PUBLIC_URL}/#home`}>
-        {`<${mainBody.firstName} />`}
+      <Navbar.Brand className="brand" href="/#home">
+        {`< Portfolio />`}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link
-            className="nav-link lead"
-            href={about.resume}
-            target="_blank"
-            rel="noreferrer noopener"
+            href="#"
+            onclick="window.open('pdf', '_blank', 'fullscreen=yes'); return false;"
           >
             Resume
           </Nav.Link>
-          <Nav.Link
-            className="nav-link lead"
-            href={`${process.env.PUBLIC_URL}/#aboutme`}
-          >
+          <Nav.Link className="nav-link lead" href="/#aboutme">
             About
           </Nav.Link>
-          <Nav.Link
-            className="nav-link lead"
-            href={`${process.env.PUBLIC_URL}/#projects`}
-          >
+          <Nav.Link className="nav-link lead" href="/#projects">
             Projects
           </Nav.Link>
-          <Nav.Link
-            className="nav-link lead"
-            href={`${process.env.PUBLIC_URL}/#skills`}
-          >
+          <Nav.Link className="nav-link lead" href="/#skills">
             Skills
           </Nav.Link>
         </Nav>
