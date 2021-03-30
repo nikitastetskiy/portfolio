a3ebed4434319eaebee9cc651927a13cddc7a73c
Nikita Stetskiy <nikin929@gmail.com>
2021-03-30 14:37:47 +0200

    Including index.js #6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
