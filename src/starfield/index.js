2be6c6178b540aef0de5071475f95fc09903b94a
Nikita Stetskiy <nikin929@gmail.com>
2021-04-05 12:34:42 +0200

    Optimizing Starfield (changing order) #6

--- src/starfield/index.js
+++ src/starfield/index.js
@@ -97,7 +97,7 @@ class StarfieldAnimation extends PureComponent {
       alphaFactor,
       lineWidth
     } = this.props
-    
+
     ctx.lineWidth = lineWidth
     
     ctx.save()
@@ -107,7 +107,7 @@ class StarfieldAnimation extends PureComponent {
 
     for (let i = 0; i < this._particles.length; ++i) {
       const p = this._particles[i]
-
+      
       p.s = this._bounds.depth / (this._bounds.depth + p.z)
       p.sx = p.x * p.s
       p.sy = p.y * p.s
@@ -118,6 +118,7 @@ class StarfieldAnimation extends PureComponent {
       ctx.lineTo(p.osx, p.osy)
       ctx.strokeStyle = 'hsla(' + p.hue + ', 100%, ' + p.lightness + '%, ' + p.alpha + ')'
       ctx.stroke()
+      
       p.update()
     }
 
